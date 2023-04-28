67dfe8f67a4c07f59859d698ab9f3e5a6f6ccee0
namiaginyan <namiaginyan@mail.tau.ac.il>
2023-04-29 00:59:03 +0300

    main

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
-import csv
-import json
-import socket
-
 import requests
 import argparse
 import urllib3
 import pandas as pd
+import csv
+import json
 
 
 def grade_calculator(url):
-
     urllib3.disable_warnings()
-    grade = 0
+    sum = 0
     headers = []
     response = requests.get(url, verify=False)
 
     if "Content-Security-Policy" in response.headers:
-        grade += 1
+        sum += 1
         headers.append("Content-Security-Policy")
 
     if response.headers.get('X-Frame-Options') and response.headers['X-Frame-Options'].lower() == 'sameorigin':
-        grade += 1
+        sum += 1
         headers.append('X-Frame-Options')
 
     if response.headers.get('Referrer-Policy') and response.headers[
         'Referrer-Policy'].lower() == 'strict-origin-when-cross-origin':
-        grade += 1
+        sum += 1
         headers.append('Referrer-Policy')
 
     if 'X-Content-Type-Options' in response.headers:
-        grade += 1
+        sum += 1
         headers.append('X-Content-Type-Options')
 
     if 'Permissions-Policy' in response.headers:
-        grade += 1
+        sum += 1
         headers.append('Permissions-Policy')
 
+    if 'Strict-Transport-Security' in response.headers:
+        sum += 1
+        headers.append('Strict-Transport-Security')
+
+    if sum == 6 or sum == 5:
+        grade = "A"
+    elif sum == 4:
+        grade = "B"
+    elif sum == 3:
+        grade = "C"
+    else:
+        grade = "D"
+
     return grade, headers
+
 
 def challenge():
     urls = []
     report = []
 
+    print(" please enter --URL for a list or --path for a file")
     parser = argparse.ArgumentParser(description='Get the URLs in command line or text file')
     parser.add_argument('--URL', nargs='*', help="list of URLs")
     parser.add_argument('--path', help="text file path containing URLS")
@@ -56,10 +68,11 @@ def challenge():
 
     for url in urls:
         info = grade_calculator(url)
-        report.append([url, info[0], info[1]])
-        # dict.append({'URL': url, 'GRADE':grade_calculator(url)})
+        grade = info[0]
+        headers = info[1]
+        report.append([url, grade, headers])
 
-    fields = ['Site','Grade', 'Headers']
+    fields = ['Site', 'Grade', 'Headers']
 
     # Creating a data frame using pandas
     df = pd.DataFrame(report, columns=fields)
@@ -77,6 +90,8 @@ def challenge():
     # Creating a json file and writing the data
     with open("report.json", "w") as outfile:
         json.dump(dict, outfile, indent=4)
+
+    print("Done! File created ")
 
 
 if __name__ == '__main__':
